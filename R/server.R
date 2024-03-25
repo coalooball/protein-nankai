352ea38c25b88f0d26c2e9a08fdcf60c6a816fcc
coalooball <cyan_cg@outlook.com>
2024-03-25 22:21:10 +0800

    Add mzml_files1 selector

--- R/server.R
+++ R/server.R
@@ -15,11 +15,10 @@ server <- function(input, output) {
     })
     
     shinyFileChoose(input, "pepxml_file1", root = get_file_root(), filetypes = c('xml'))
+    shinyFileChoose(input, "mzml_files1", root = get_file_root(), filetypes = c('mzML'))
     pepxml_name <- reactive({
         # print(typeof(input$pepxml_file1))
         # if (typeof(input$pepxml_file1) == "list") {
-            print(input$pepxml_file1)
-            print(get_file_root())
         if (!is.null(input$pepxml_file1)) {
             file_selected<-parseFilePaths(get_file_root(), input$pepxml_file1)
             # https://stackoverflow.com/questions/25640161/r-how-to-test-for-character0-in-if-statement
@@ -33,19 +32,60 @@ server <- function(input, output) {
             ""
         }
     })
-    output$pepxml_file1_contents <- renderTable({
-        ext <- tools::file_ext(pepxml_name())
-
-        req(file)
-        validate(need(ext == "xml", "Please upload a pep.xml file"))
-        # pepXML2tab(file$datapath)
+    mzML_name <- reactive({
+        if (!is.null(input$mzml_files1)) {
+            file_selected<-parseFilePaths(get_file_root(), input$mzml_files1)
+            # https://stackoverflow.com/questions/25640161/r-how-to-test-for-character0-in-if-statement
+            # The type of file_selected$datapath is `character(0)` at very beginning. 
+            if (rlang::is_empty(file_selected$datapath)) {
+                ""
+            } else {
+                print(typeof(file_selected$datapath))
+                print(file_selected$datapath)
+                as.character(file_selected$datapath)
+            }
+        } else {
+            ""
+        }
     })
+    observeEvent(input$analyzing_data, {
+        # output$bool_analyzing <- reactive({TRUE})
+        output$analyzing_info <- renderText({ 
+            paste0("Loading Pepxml File: ", pepxml_name(), " ...")
+        });
+        output$pepxml_file1_contents <- renderTable({
+            
+            ext <- tools::file_ext(pepxml_name())
+
+            req(file)
+            validate(need(ext == "xml", "Please upload a pep.xml file"))
+            
+            pepXML2tab(pepxml_name())
+        })
+        output$analyzing_info <- renderText({ 
+            paste0("Loaded Pepxml File: ", pepxml_name(), " .")
+        });
+    })
+    output$bool_analyzing <- reactive({FALSE})
     output$is_selected_pepxml_file1 <- reactive({
         nchar(pepxml_name()) > 0
     })
+    output$is_selected_mzML_file1 <- reactive({
+        nchar(mzML_name()) > 0
+    })
     output$pepxml_file1_path_detail <- renderText({ 
-        print(pepxml_name())
         paste0("Selected Pepxml File: ", pepxml_name(), " .")
     });
+    output$mzml_file1_path_detail <- renderText({ 
+        print(mzML_name())
+        paste0("Selected mzML Files: ", mzML_name(), " .")
+    });
+    # When the pepxml mzml file is also selected, the Analyze button is displayed
+    output$show_analyzing_btn <- reactive({
+        nchar(pepxml_name()) > 0 && nchar(mzML_name()) > 0
+    })
     outputOptions(output, "is_selected_pepxml_file1", suspendWhenHidden = FALSE)  
+    outputOptions(output, "is_selected_mzML_file1", suspendWhenHidden = FALSE)  
+    outputOptions(output, "show_analyzing_btn", suspendWhenHidden = FALSE)  
+    # outputOptions(output, "bool_analyzing", suspendWhenHidden = FALSE)  
 }
--- R/ui.R
+++ R/ui.R
@@ -15,6 +15,31 @@ ui <- fluidPage(
         )
     ),
     fluidRow(
+        column(6,
+            shinyFilesButton("mzml_files1", label="Choose mzML Files", title='Please select mzML files', multiple=TRUE)
+        ),
+        column(6,
+            conditionalPanel(
+            condition = "output.is_selected_mzML_file1",
+            textOutput("mzml_file1_path_detail")
+            )
+        )
+    ),
+    conditionalPanel(
+        condition = "output.show_analyzing_btn",
+        fluidRow(
+            column(2,
+                actionButton("analyzing_data", "Analyzing", class = "btn-success")
+            ),
+            column(6,
+                # conditionalPanel(
+                    # condition = "output.bool_analyzing",
+                    textOutput("analyzing_info")
+                # )
+            )
+        )
+    ),
+    fluidRow(
         tableOutput("pepxml_file1_contents")
     )
 )
